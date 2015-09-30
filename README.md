e4b62c0f4caf5b06459140441d07ea606af0eaa9
Cris Favero <marioatlp@gmail.com>
2015-09-30 10:12:28 -0400

    initial code push

---
+++ README.md
@@ -0,0 +1,7 @@
+# WaterCycle
+
+### This will be a simple collection of aws cloud formation scripts to easily launch 
+
+Usage: `createStack configuration.json`
+
+*This is the very beginning of this script so things are likely to change drastically* 
---
+++ createStack.js
@@ -0,0 +1,47 @@
+/**
+ * Created by cmfavero on 9/29/15.
+ */
+var fs = require('fs');
+var path = require('path');
+var AWS = require('aws-sdk');
+
+var cloudFormation = new AWS.CloudFormation({region: "us-east-1"});
+var options = process.argv.slice(2);
+
+if (options.length !== 1) {
+	console.error("Expecting only a json configuration file name");
+	process.exit(-1)
+}
+
+try {
+	var configFile = require(path.resolve(options[0]));
+} catch (e) {
+	console.error("could not load json file", e.stack || e);
+	process.exit(-1)
+}
+
+if (!configFile.templateFileName || configFile.stackName)
+	var templateBody = fs.readFileSync(configFile.templateFileName);
+
+var stack = {
+	"StackName": configFile.stackName,
+	"TemplateBody": templateBody.toString('utf8')
+};
+
+if (configFile.capabilities && configFile.capabilities.length > 0) {
+	stack.Capabilities = configFile.capabilities;
+}
+if (configFile.parameters && configFile.parameters.length > 0) {
+	stack.Parameters = configFile.parameters;
+}
+
+function response(err, data) {
+	if (err) {
+		// an error occurred
+		console.error(err, err.stack);
+	} else {
+		// successful response
+		console.log(data);
+	}
+}
+cloudFormation.createStack(stack, response);
